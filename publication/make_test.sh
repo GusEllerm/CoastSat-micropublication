f87f21602386c7fdaa3b46bef8fa929a493b67cc
Augustus Ellerm <ellermaugustus@gmail.com>
2025-07-16 10:32:39 +1200

    update

--- publication/make_test.sh
+++
@@ -1,8 +0,0 @@
-echo "📄 Generating DNF Document..."
-stencila convert test.smd DNF_Document.json
-
-echo "📑 Rendering DNF Document with interface.crate..."
-stencila render DNF_Document.json DNF_Evaluated_Document.json --force-all --pretty 
-
-echo "📊 Creating example presentation versions of the rendered article..."
-stencila convert DNF_Evaluated_Document.json test.html --pretty 
